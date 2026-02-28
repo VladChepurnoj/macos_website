5d40b9cb2cc739240f0ad9886c72214d636ad7a4
Vlad <chepurnojv@gmail.com>
2026-02-28 05:27:58 +0200

    window controls

--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -2,6 +2,7 @@ import { useLayoutEffect, useRef } from "react";
 import { useWindowStore } from "#store/window";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
 
 const WindowWrapper = (Component, windowKey) => {
   const Wrapped = (props) => {
@@ -25,6 +26,17 @@ const WindowWrapper = (Component, windowKey) => {
         { scale: 1, opacity: 1, y: 0, duration: 0.4, ease: "power3.out" },
       );
     }, [isOpen]);
+
+    useGSAP(() => {
+      const el = ref.current;
+      if (!el) return;
+
+      const [instance] = Draggable.create(el, {
+        onPress: () => focusWindow(windowKey),
+      });
+
+      return () => instance.kill();
+    }, []);
 
     useLayoutEffect(() => {
       const el = ref.current;
